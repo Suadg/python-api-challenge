71ef38463ec767859dab3142774d2bd7c804958a
Suad G <162393107+Suadg@users.noreply.github.com>
2024-04-11 19:18:23 -0400

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "c7e85fb7c13a95990d804b187fec5464"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "57f25cfc007847cfad2e6d018f24ab0b"
